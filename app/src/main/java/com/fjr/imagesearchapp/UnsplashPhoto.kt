e40fc7b017f493d5584dccbf175ecda177adafd6
Franky Wijanarko <frank.jr.619@gmail.com>
2021-09-26 01:12:59 +0700

    layout and model class

---
+++ app/src/main/java/com/fjr/imagesearchapp/UnsplashPhoto.kt
@@ -0,0 +1,35 @@
+package com.fjr.imagesearchapp
+
+
+/**
+ * Created by Franky Wijanarko on 26/09/21.
+ * frank.jr.619@gmail.com
+ */
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UnsplashPhoto(
+    val id: String,
+    val description: String?,
+    val urls: UnsplashPhotoUrls,
+    val user: UnsplashUser
+) : Parcelable {
+
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        val raw: String,
+        val full: String,
+        val regular: String,
+        val small: String,
+        val thumb: String,
+    ) : Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+        val name: String,
+        val username: String
+    ) : Parcelable {
+        val attributionUrl get() = "https://unsplash.com/$username?utm_source=ImageSearchApp&utm_medium=referral"
+    }
+}
