22a91bdb88a276255891bcd59895d21005123796
Franky Wijanarko <frank.jr.619@gmail.com>
2021-09-26 13:33:50 +0700

    search functionallity

--- app/src/main/java/com/fjr/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/fjr/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -1,7 +1,10 @@
 package com.fjr.imagesearchapp.ui.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.fjr.imagesearchapp.R
@@ -40,6 +43,32 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
 
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_gallery, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                if (query != null) {
+                    binding.recyclerView.scrollToPosition(0)
+                    viewModel.searchPhotos(query)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
     }
 
     override fun onDestroyView() {
