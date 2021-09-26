0189eff75d9ba1334da1fe89bdd125dc6abf082b
Franky Wijanarko <frank.jr.619@gmail.com>
2021-09-26 14:48:33 +0700

    handling process death

--- app/src/main/java/com/fjr/imagesearchapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/fjr/imagesearchapp/ui/gallery/GalleryViewModel.kt
@@ -1,12 +1,10 @@
 package com.fjr.imagesearchapp.ui.gallery
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.PagingSource
 import androidx.paging.cachedIn
 import com.fjr.imagesearchapp.data.UnsplashRepository
+import dagger.assisted.Assisted
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -18,10 +16,12 @@ import javax.inject.Inject
 
 @HiltViewModel
 class GalleryViewModel @Inject constructor(
-    private val repository: UnsplashRepository
+    private val repository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
 ) : ViewModel() {
 
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
+
     val photos = currentQuery.switchMap { queryString ->
        repository.getSearchResult(queryString).cachedIn(viewModelScope)
     }
@@ -31,6 +31,7 @@ class GalleryViewModel @Inject constructor(
     }
 
     companion object {
+        private const val CURRENT_QUERY = "current_query"
         private const val DEFAULT_QUERY = "cats"
     }
 }
